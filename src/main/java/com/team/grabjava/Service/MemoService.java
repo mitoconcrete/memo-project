ec6016824915f0aa277044a60696610afac5b08b
jeongtu <dohmjid@gmail.com>
2022-11-23 21:29:22 +0900

    JavaMemoService, MemoService commit

--- src/main/java/com/team/grabjava/Service/MemoService.java
+++ src/main/java/com/team/grabjava/Service/MemoService.java
@@ -1,4 +1,13 @@
 package com.team.grabjava.Service;
 
+import com.team.grabjava.Entities.Memo;
+import java.util.List;
+
 public class MemoService {
+
+    boolean saveMemo(String writer, String password, String memo);
+    boolean updateMemo(int id, String password, String updatePayload);
+    boolean deleteMemo(int id, String password);
+    List<Memo> getMemoList();
+    boolean hasMemoFindById(int id);
 }
